17f44571c83da5a9623be9bf706ebdb9ebe9c64f
romano2717 <romano2717@gmail.com>
2015-04-28 17:59:50 +0800

    first commit for comr12

---
+++ comress/SurveyTableViewCell.m
@@ -0,0 +1,69 @@
+//
+//  SurveyTableViewCell.m
+//  comress
+//
+//  Created by Diffy Romano on 6/4/15.
+//  Copyright (c) 2015 Combuilder. All rights reserved.
+//
+
+#import "SurveyTableViewCell.h"
+
+@implementation SurveyTableViewCell
+
+@synthesize numOfQuestions;
+
+- (void)awakeFromNib {
+    // Initialization code
+    
+    questions = [[Questions alloc] init];
+    
+    numOfQuestions = (int)[[questions questions] count];
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+- (void)initCellWithResultSet:(NSDictionary *)dict
+{
+    @try {
+        NSDictionary *survey = [dict objectForKey:@"survey"];
+        NSDictionary *address = [dict objectForKey:@"address"];
+        NSArray *answers = [dict objectForKey:@"answers"];
+        
+        if(survey != nil)
+        {
+            if([survey valueForKey:@"resident_name"] != [NSNull null])
+                self.residentName.text = [survey valueForKey:@"resident_name"];
+            
+            double timeStamp = [[survey valueForKey:@"survey_date"] doubleValue];
+            NSDate *date = [NSDate dateWithTimeIntervalSince1970:timeStamp];
+            
+            NSDateFormatter *format = [[NSDateFormatter alloc] init];
+            [format setDateFormat:@"dd-MMM-YYYY h:mm"];
+            NSString *datestring = [format stringFromDate:date];
+            
+            self.dateLabel.text = [NSString stringWithFormat:@"%@",datestring];
+            
+            self.satisfactionRatingLabel.text = [NSString stringWithFormat:@"%.2f%% Satisfaction",[[survey valueForKey:@"average_rating"] floatValue]];
+        }
+        
+        
+
+        if(address != nil)
+        {
+            if([survey valueForKey:@"address"] != [NSNull null])
+                self.addressLabel.text = [address valueForKey:@"address"];
+        }
+    }
+    @catch (NSException *exception) {
+        DDLogVerbose(@"NSException %@",exception);
+    }
+    @finally {
+
+    }
+}
+
+@end
